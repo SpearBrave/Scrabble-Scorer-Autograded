1596a7ba396389f05810272389028b4d83bcc581
SpearBrave <gerardovazquez9509@gmail.com>
2024-02-08 03:06:44 -0600

    final

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 const scorer = require('./scrabble-scorer');
 
 scorer.runProgram();
-// Recall that for...in loops iterate over the keys within an object.
-
-// If you need a reminder of how to assign new key/value pairs, review the relevant section in the Objects and Math chapter.
-
-// To access the letter arrays within oldPointStructure, use bracket notation (oldPointStructure['key']).
-
-// To access a particular element within a letter array, add a second set of brackets (oldPointStructure['key'][index]), or assign the array to a variable and use variableName[index].
+    
